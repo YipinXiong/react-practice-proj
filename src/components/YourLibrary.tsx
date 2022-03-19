29d2c37ad12269fd7d8547da68871b747ba9157e
Yipin Xiong <xiong1pin@gmail.com>
2022-03-19 12:13:30 +1100

    resize play around

--- src/components/YourLibrary.tsx
+++ src/components/YourLibrary.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 const YourLibrary = () => {
+  const [leftWidth, setLeftWidth] = useState('50%');
+  const resizeParentRef = useRef<HTMLDivElement>(null);
+  const onMouseMove = (e: MouseEvent) => {
+    const finalWidth = e.clientX - 200;
+    setLeftWidth(`${finalWidth}px`)
+  };
+  const onMouseDownHandler = () => {
+    resizeParentRef?.current?.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', () => {
+      resizeParentRef?.current?.removeEventListener('mousemove', onMouseMove);
+    })
+  };
   return (
-      <div>
-        Your Library
+      <div ref={resizeParentRef} className="d-flex" style={{height: '100%'}}>
+        <div className="border-end position-relative" style={{width: leftWidth, background: 'red'}}>
+          <div onMouseDown={onMouseDownHandler} className="position-absolute"
+               style={{
+                 top: 0,
+                 right: 0,
+                 bottom: 0,
+                 height: '100%',
+                 width: '10px',
+                 backgroundColor: 'blue',
+                 cursor: 'ew-resize'
+               }}/>
+        </div>
+
+        <div style={{flex: 1, background: 'greenyellow'}}/>
       </div>
   );
 };
