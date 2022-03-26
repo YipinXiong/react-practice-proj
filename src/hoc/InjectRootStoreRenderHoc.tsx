d569b7ccac956a2758839837ad0d5ed3f2bcc0bf
Yipin Xiong <xiong1pin@gmail.com>
2022-03-26 19:58:20 +1100

    added comments

--- src/hoc/InjectRootStoreRenderHoc.tsx
+++ src/hoc/InjectRootStoreRenderHoc.tsx
@@ -3,6 +3,10 @@ import {Fragment} from 'react';
 import {useRootStore} from '../hooks/app-root-store.store';
 import RootStore from '@root-store';
 
+/**
+ * @param props: a callback fn that will return ReactElement which needs to inject rootStore from props
+ * This function is just a practice of the `render prop` concept
+ */
 const InjectRootStoreRenderHoc = (props: { renderWithStore: (rootStore: RootStore) => React.ReactElement }) => {
   const rootStore = useRootStore();
   return (
--- src/hoc/withRootStoreHOC.tsx
+++ src/hoc/withRootStoreHOC.tsx
@@ -10,7 +10,7 @@ interface WithRootStoreInProps {
 type ExcludedRootStoreInProps<P> = Omit<P, keyof WithRootStoreInProps>;
 
 function withRootStore<P extends WithRootStoreInProps = WithRootStoreInProps>(WrappedComponent: ComponentType<P>): ComponentType<ExcludedRootStoreInProps<P>> {
-  /* This is the bug that you need to as P and spread it ahead of rootStore*/
+  /* This is a bug that you need to `as P` and spread it ahead of rootStore*/
   return function withRootStoreComp(props: ExcludedRootStoreInProps<P>) {
     const rootStore = useRootStore();
     return <WrappedComponent {...props as P} rootStore={rootStore}/>;
